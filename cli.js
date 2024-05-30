90ec3d3d0aa58a8902387512b4b3b56437a90b84
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2024-05-30 04:23:51 -0600

    [FEAT]Practica

--- cli.js
+++ cli.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
+const express = require('express');
+
 const mongoose = require('mongoose');
+
+const server = express();
+
+server.use(express.json());
+
+const port = 8080
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME }= process.env;
 const MONGO_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
@@ -34,26 +42,41 @@ const Koder = mongoose.model("koder", new mongoose.Schema({
     },
 }));
 
+server.post('/koders', (req, res) => {
+
+
 mongoose.connect(MONGO_URI)
 .then(() => {
     console.log("Conexion exitosa");
+    
+    const { firstName, lastName, email, birthDate, generation } = req.body;
     // insertar
     Koder.create({
-        firstName: "Yair",
-        lastName: "Guadarrama",
-        email: "yair@kodemia.mx",
-        birthDate: new Date("1995-11-11"),
-        generation: 33,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        birthDate: birthDate,
+        generation: generation,
     })
-    .then(() => console.log("Koder created"))
+    .then(() => {
+        console.log("Koder created")
+        res.json({
+            message: "koder añadido",
+        })
+    })
     .catch((error) => console.error("Error al crear Koder", error));
 })
 .catch ((error) => {
     console.error("Error al conectar con la base de datos", error);
 });
+});
 
 /*
-Las pronmesas por defecto nacen o se crean en estado - pendiente 
+Las promesas por defecto nacen o se crean en estado - pendiente 
 - resolve: se resuelve la promesa (then) se ejcuta cuando pasa de pendiente a resuelta
 - reject: se rechaza la promesa (catch) pendiente a rechazada
 */
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
